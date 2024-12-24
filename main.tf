ffc9c0935ba91a2bc926ff2381dd5ee9687cb2b0
unknown <krsameersingh05@gmail.com>
2024-12-24 16:15:28 +0530

    delete

--- main.tf
+++ main.tf
@@ -1,15 +1,1 @@
-resource "aws_instance" "dev" {
-  ami                     = "ami-036841078a4b68e14"
-instance_type = "t2.micro"
-tags = {
-  name = "webserver"
-}
-  } 
-
-resource "aws_instance" "pro" {
-  ami                     = "ami-036841078a4b68e14"
-instance_type = "t2.micro"
-tags = {
-  name = "webserver"
-}
-  } 
+ 
