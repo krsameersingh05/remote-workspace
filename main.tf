7c6770500fc0daa29f409898ef5c21cee4d7bbfd
krsameersingh05 <168332772+krsameersingh05@users.noreply.github.com>
2024-12-24 16:48:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,1 @@
- resource "aws_instance" "dev" {
-  ami                     = "ami-0b4624933067d393a"
-instance_type = "t2.micro"
-tags = {
-  name = "webserver"
-}
-}
-
- resource "aws_instance" "pro"  {
-  ami                     = "ami-0b4624933067d393a"
-instance_type = "t2.micro"
-tags = {
-  name = "webserver"
-}
-}
 
